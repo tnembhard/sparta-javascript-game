8df8b62cdbd2b06c1f9f1a58d9d48f916754d099
tnembhard <tnembhard@spartaglobal.com>
2019-01-23 16:46:15 +0000

    minimum viable product

--- js/app.js
+++ js/app.js
@@ -1,109 +1,141 @@
-
-class Bird {
+class Bird{
     constructor(){
         this.grid = document.getElementsByClassName(`grids`);
-        this.coin = document.createElement(`img`)
+        this.coin = document.createElement(`img`);
         this.coin.setAttribute(`id`, `coin`); 
         this.coin.src = `/images/coin.png`; 
-        this.bird = document.createElement(`img`) 
-        this.bird.src = `/images/bird.png`;
-        this.coinClick;
-        this.box = document.getElementById("box");   
+        this.bird = document.createElement(`img`); 
+        this.bird.src = `/images/bird.png`; 
+        this.bird.style.position = "absolute";
+        this.box = document.getElementById("box");
+        this.interval;
+        this.click = document.createElement(`div`); 
+        this.score = 0;
+        this.scoreTxt = document.getElementById(`score`)
+        this.gameBtn = document.getElementById(`gameBtn`);    
     }
     
     gridItems() {    
         // Creates the grid for the bird and coin 
-        const inputRow = 18; 
-        const inputCol = 18; 
-    
+        const inputRow = 28; 
+        const inputCol = 18;     
         // Row Creator
         for (let i = 0; i < inputRow; i++) {
           const row = document.createElement(`div`);
           row.setAttribute(`class`, `row`);
-          this.box.appendChild(row);
-      
+          this.box.appendChild(row);      
           // Collumn Creator
           for (let j = 0; j < inputCol; j++) {
             const col = document.createElement(`div`);
-            col.setAttribute(`class`, `col grids`);            
-            // col.innerText=`${j}`;
+            col.setAttribute(`class`, `col grids`); 
             row.appendChild(col) 
           }    
         }   
     }    
 
-    imgPos(image){ 
-        // Randomly attaches the coin image to a cell in the grid     
-          
-        let num = Math.floor(Math.random() * Math.floor(this.grid.length));
+    imgGrid(image){ 
+        // Randomly attaches the coin image to a cell in the grid  
+        let num = Math.floor(Math.random() * this.grid.length);
         this.grid[num].appendChild(image);
-        console.log(num);
-        let valY;
-        let valX;
-
-        function getPos(item) {
-            let rect=item.getBoundingClientRect();
-            valY = rect.top;
-            valX = rect.left;
-            return {x:rect.left,y:rect.top};  
+        if (image === this.coin) {
+            this.coinPos(this.grid[num]);
+        } else if (image === this.click){
+            this.clickPos(this.grid[num]);
         }
-        console.log(getPos(this.grid[num]));
-        getPos(this.grid[num]);
-        this.placeBird(valX, valY);
+    }
+
+    coinPos(img) {
+        //Function to get the coordinates of the coin.
+        let rect=img.getBoundingClientRect();
+        this.coinY = rect.top;
+        this.coinX = rect.left;            
+    }
+
+    clickPos(img) {
+        //Function to get random coordinates to help ramdomly generate the crows location.
+        let rect=img.getBoundingClientRect();
+        this.clickY = rect.top;
+        this.clickX = rect.left;            
     }
 
     birdMove(){
-        // Method to test moving the bird using css and intervals
+        //Function that moves the birds position to the coin.
         this.box.appendChild(this.bird);
-        this.bird.setAttribute(`id`, `bird`);    
+        this.interval = setInterval( () => { 
+            if (Math.round(this.birdX) < Math.round(this.coinX)) {
+                this.birdX += 1;
+                this.bird.style.left = this.birdX+'px';           
+            } if (Math.round(this.birdX) > Math.round(this.coinX)){
+                this.birdX -= 1;
+                this.bird.style.left = this.birdX+'px';
+            }  if (Math.round(this.birdY) < Math.round(this.coinY)) {    
+                this.birdY += 1;
+                this.bird.style.top = this.birdY+'px';         
+            } if (Math.round(this.birdY) > Math.round(this.coinY)){
+                this.birdY -= 1;
+                this.bird.style.top = this.birdY+'px';               
+            } this.loseCondition();  
+        }, 10); 
     }
 
-    placeBird(x, y) {
+    placeBird() {
+        // Function to randomly place the bird 
         this.bird.style.position = "absolute";
-        this.bird.style.left = x+'px';
-        this.bird.style.top = y+'px';
+        this.birdX = this.clickX;
+        this.birdY = this.clickY;
+        this.bird.style.left = this.clickX+'px';
+        this.bird.style.top = this.clickY+'px';
     }
 
-    birdMove2(){
-        // Method to move the bird using the grid and intervals
-
+    loseCondition(){
+        //Function to dictate the conditions for the user to lose the game.
+        if (Math.round(this.birdX) == Math.round(this.coinX) && Math.round(this.birdY) == Math.round(this.coinY)) {          
+            this.scoreTxt.innerText=`You lose, your score is: ${this.score} points.`;
+            clearInterval(this.interval);          
+        }
     }
 
     coinClick(){
         // Method used to click the coin before the bird reaches the coin.
-
-    }
-
-    loseCondition(){
-
+        this.coin.addEventListener(`mouseover`, event => {
+            this.imgGrid(event.target);
+            clearInterval(this.interval);
+            this.imgGrid(this.click);
+            this.placeBird();
+            this.birdMove();
+            this.score += 10;
+            this.scoreTxt.innerText=`Your score is: ${this.score} points.`;
+        });   
     }
 
     startGame(){
-        // Method to start the game
+        // Method to start and reset the game
+        while (this.box.hasChildNodes()) {
+            this.box.removeChild(this.box.lastChild);
+        }
         this.gridItems()
-        this.imgPos(newGame.coin)
-        this.birdMove()
-        // this.imgPos(newGame.bird)
+        this.gameBtn.addEventListener(`click`, () => {
+            this.imgGrid(this.coin)
+            this.imgGrid(this.click);
+            this.placeBird()
+            this.birdMove()
+            this.score = 0;         
+            this.coinClick()
+        });
     }
-
 }
 
+const modal = document.getElementById('myModal');
+const moBtn = document.getElementById(`modalBtn`);
+let mContent = document.getElementById(`modal-content`);
+moBtn.addEventListener(`click`, () => {
+    modal.style.display = "block";
+})
+window.addEventListener(`click`, event => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+})
 
 const newGame = new Bird();
-
 newGame.startGame()
-
-
-
-
-
-
-
-function showCoords(event) {
-    var x = parseFloat(event.clientX);
-    var y = parseFloat(event.clientY);
-    var coords = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("demo").innerHTML = coords;
-  }
-
-
